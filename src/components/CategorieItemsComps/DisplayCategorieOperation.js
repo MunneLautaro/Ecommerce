f887f6ef59e6a995aefa671958ac4bb9a09fc6d5
MunneLautaro <lautaro_munne@outlook.com>
2025-12-28 16:28:56 -0300

    Termino funcionalidades para modificar y borrar items, falta agregar modals de confirmacion para los mismos

--- src/components/CategorieItemsComps/DisplayCategorieOperation.js
+++ src/components/CategorieItemsComps/DisplayCategorieOperation.js
@@ -2,54 +2,31 @@
 
 import { useState, useReducer, useEffect } from "react"
 import { ItemContext, ItemDispatchContext } from "@/contexts/ItemContext"
-import {
-  deleteCategorieItemAction,
-  getCategorieItemsAction,
-} from "@/actions/categorieAction"
-import { Trash2, Edit } from "react-feather"
+import { useFetchItems } from "@/hooks/fetchItems"
 import {
   initialItems,
   itemFilterReducer,
 } from "../../reducers/categorieItemReducer"
-import { modifyCategorieAction } from "@/actions/categorieAction"
 import { toast } from "react-toastify"
 import AddCategorieItem from "./CategorieOperationForms/AddCategorieItem"
-import ModDelCategorieItemTable from "./CategorieOperationForms/ModDelCategorieItemTable"
-import ItemActionButton from "./ItemActionButton"
+import CategorieItemTableContainer from "./CategorieOperationForms/CategorieItemTableContainer"
 
 export default function CategorieItems() {
   const [displayAddItem, setDisplayAddItem] = useState(true)
-  const [response, setResponse] = useState(null)
   const [itemFilter, dispatchItemFilter] = useReducer(
     itemFilterReducer,
     initialItems
   )
+  const fetchItems = useFetchItems(dispatchItemFilter)
 
   useEffect(() => {
-    if (!response) return
-    if (response?.success) {
-      toast.success(response?.success)
-      toast.info(response?.message)
+    if (!itemFilter?.response) return
+    if (itemFilter?.response?.success) {
+      toast.success(itemFilter?.response?.message)
     } else {
-      toast.error(response?.error)
+      toast.error(itemFilter?.response?.error)
     }
-  }, [response])
-
-  const fetchItems = async () => {
-    try {
-      const items = await getCategorieItemsAction()
-
-      dispatchItemFilter({
-        type: "FETCH_SUCCESS",
-        payload: items?.data,
-      })
-    } catch {
-      dispatchItemFilter({
-        type: "FETCH_FAIL",
-        payload: "Error fetching products",
-      })
-    }
-  }
+  }, [itemFilter?.response])
 
   useEffect(() => {
     dispatchItemFilter({ type: "FETCH_INIT" })
@@ -89,40 +66,7 @@ export default function CategorieItems() {
                   <AddCategorieItem onSubmit={fetchItems} />
                 ) : (
                   <>
-                    <div className="flex flex-row items-start">
-                      <ModDelCategorieItemTable />
-                      <ItemActionButton
-                        disabled={!itemFilter?.newValue}
-                        onClick={async (e) => {
-                          const modItemResponse = await modifyCategorieAction(
-                            itemFilter?.currentItem?.type,
-                            itemFilter?.currentItem?.value,
-                            itemFilter?.newValue
-                          )
-                          setResponse(modItemResponse)
-                          fetchItems()
-                          dispatchItemFilter({ type: "RESET_CURRENT_ITEM" })
-                        }}
-                      >
-                        <Edit />
-                      </ItemActionButton>
-                      <ItemActionButton
-                        disabled={!itemFilter?.currentItem}
-                        onClick={async (e) => {
-                          e.preventDefault()
-                          const delItemResponse =
-                            await deleteCategorieItemAction(
-                              itemFilter?.currentItem?.type,
-                              itemFilter?.currentItem?.value
-                            )
-                          setResponse(delItemResponse)
-                          fetchItems()
-                          dispatchItemFilter({ type: "RESET_CURRENT_ITEM" })
-                        }}
-                      >
-                        <Trash2 />
-                      </ItemActionButton>
-                    </div>
+                    <CategorieItemTableContainer />
                   </>
                 )}
               </div>
